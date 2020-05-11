2f7b2f45481cf0a6de24c613904bc1d65ad703e5
James Shen <james.shen@guidebee.com>
2020-05-11 20:22:10 +0800

    update company info

--- scripts/update_company_info.py
+++ scripts/update_company_info.py
@@ -5,10 +5,10 @@ import sqlite3
 db_file = '../../asx_gym/asx_gym/db.sqlite3'
 
 create_directory_if_not_exist('data/company')
-# company_file = 'company/companies.json'
-# download_file(company_file)
-# sector_file = 'company/sectors.json'
-# download_file(sector_file)
+company_file = 'company/companies.json'
+download_file(company_file)
+sector_file = 'company/sectors.json'
+download_file(sector_file)
 
 conn = sqlite3.connect(db_file)
 
