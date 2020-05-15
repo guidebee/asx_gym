d64ddb67a1c70091029214b0db5b1309b884d42c
James Shen <james.shen@guidebee.com>
2020-05-15 11:33:01 +0800

    test dash ploty

--- app/dash_plotly/bootstrap_app.py
+++ app/dash_plotly/bootstrap_app.py
@@ -1,11 +1,26 @@
-import dash_bootstrap_components as dbc
+# Dash packages
+from django_plotly_dash import DjangoDash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 
-from django_plotly_dash import DjangoDash
+# Plotly packages
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+# Data manipulation packages
+import numpy as np
+import pandas as pd
 
 app = DjangoDash('BootstrapApplication')  # replaces dash.Dash
+
+df = px.data.gapminder()
+fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
+                 size="pop", color="continent", hover_name="country",
+                 log_x=True, size_max=55, range_x=[100, 100000], range_y=[25, 90])
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -16,14 +31,6 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
+        figure=fig
     )
 ])
