1a676e3db84f6b25fb7bfa53ac3b23fd112a5e69
James Shen <james.shen@guidebee.com>
2020-05-21 22:50:26 +0800

    draft render use mpt finance

--- asx_gym/envs/asx_gym_env.py
+++ asx_gym/envs/asx_gym_env.py
@@ -1,6 +1,7 @@
 import numpy as np
 import io
 from time import sleep
+from datetime import datetime, timedelta
 import cv2
 from gym.envs.classic_control import rendering
 from matplotlib.image import imread
@@ -8,20 +9,27 @@ import matplotlib.pyplot as plt
 import gym
 from gym import spaces
 from gym.utils import seeding
+import sqlite3
+from gym.utils.colorize import *
+# Data manipulation packages
+import pandas as pd
+import pathlib
+import mplfinance as mpf
 
-plt.style.use('seaborn-pastel')
+date_fmt = '%Y-%m-%d'
+
+plt.xticks(rotation=90)
 
 
 # define a function which returns an image as numpy array from figure
 def get_img_from_fig(fig, dpi=180):
     buf = io.BytesIO()
-    fig.savefig(buf, format="png", dpi=dpi)
+    fig.savefig(buf, format="png", dpi=dpi, papertype='a4')
     buf.seek(0)
     img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
     buf.close()
     img = cv2.imdecode(img_arr, 1)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
     return img
 
 
@@ -32,6 +40,31 @@ class AsxGymEnv(gym.Env):
         self.step_count = 0
 
         self.viewer = rendering.SimpleImageViewer()
+        print(colorize("Initializing data, it may take a couple minutes,please wait...", 'red'))
+        db_file = f'{pathlib.Path().absolute()}/asx_gym/db.sqlite3'
+        con = sqlite3.connect(db_file)
+        print(colorize("reading asx index data", 'blue'))
+        self.index_df = pd.read_sql_query(
+            'SELECT index_date as Date,open_index as Open,close_index as Close,high_index as High,low_index as Low FROM stock_asxindexdailyhistory where index_name="ASX100"  order by index_date',
+            con,
+            parse_dates={'Date': date_fmt}, index_col=['Date'])
+
+        print(f'Asx index records:\n{self.index_df.count()}')
+        print(colorize("reading asx company data", 'blue'))
+        self.company_df = pd.read_sql_query('SELECT id,name,description,code,sector_id FROM stock_company', con)
+        print(f'Asx company count:\n{self.company_df.count()}')
+        print(colorize("reading asx sector data", 'blue'))
+        self.sector_df = pd.read_sql_query('SELECT id,name,full_name FROM stock_sector', con)
+        print(f'Asx sector count:\n{self.sector_df.count()}')
+        # print(colorize("reading asx stock data, please wait...", 'blue'))
+        # self.price_df = pd.read_sql_query(
+        #     f'SELECT * FROM stock_stockpricedailyhistory order by price_date', con,
+        #     parse_dates={'price_date': date_fmt})
+        # print(f'Asx stock data records:\n{self.price_df.count()}')
+        con.close()
+        print(colorize("Data initialized", "green"))
+        self.start_date = datetime(2012, 1, 1)
+        self.display_days = 60
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -39,28 +72,56 @@ class AsxGymEnv(gym.Env):
 
     def step(self, action):
         self.ax.clear()
-        x = np.linspace(-np.pi, np.pi)
+
         self.step_count += 1
-        self.ax.plot(x, np.sin(x - self.step_count / (2 * np.pi)), label="sin", color='r')
-        self.ax.set_title(f"sin(x) with {self.step_count}")
+        # start_date = self.start_date + timedelta(days=self.step_count)
+        # end_date = self.start_date + timedelta(days=self.step_count + self.display_days)
+        #
+        # self.ax.set_xlim(start_date, end_date)
+        # self.ax.set_ylim(0, 10000)
+        # self.ax.set_xlabel("Date")
+        #
+        # self.ax.set_ylabel("Index")
+        # self.ax.clear()
+        #
+        # stock_index = self.index_df.loc[start_date:end_date]
+        #
+        # self.ax.plot(stock_index.index, stock_index['Close'])
+        # self.ax.set_title(f"Asx Index {self.step_count}")
+
+        start_date = self.start_date + timedelta(days=self.step_count)
+        end_date = self.start_date + timedelta(days=self.step_count + self.display_days)
+        stock_index = self.index_df.loc[start_date:end_date]
+        self.fig, self.ax = mpf.plot(stock_index, type='candle',returnfig=True)
+        # self.ax.set_xlim(start_date, end_date)
+        # self.ax.set_ylim(0, 10000)
+        # self.ax.set_xlabel("Date")
+        # self.ax.set_ylabel("Index")
+        #
+        # self.ax.set_title(f"Asx Index {self.step_count}")
+
         done = False
         if self.step_count > 360:
             done = True
         return self.step_count, 0, done, {}
 
     def reset(self):
-        x = np.linspace(-np.pi, np.pi)
-        self.ax.set_xlim(-np.pi, np.pi)
-        self.ax.set_xlabel("x")
-        self.ax.set_ylabel("y")
-        self.ax.clear()
-        self.step_count = 0
 
-        self.ax.plot(x, np.sin(x - self.step_count / (2 * np.pi)), label="sin", color='b')
-        self.ax.set_title(f"sin(x) with {self.step_count}")
+        self.step_count = 0
+        start_date = self.start_date + timedelta(days=self.step_count)
+        end_date = self.start_date + timedelta(days=self.step_count + self.display_days)
+        stock_index = self.index_df.loc[start_date:end_date]
+        self.fig, self.ax = mpf.plot(stock_index, type='candle',returnfig=True)
+        # self.ax.set_xlim(start_date, end_date)
+        # self.ax.set_ylim(0, 10000)
+        # self.ax.set_xlabel("Date")
+        # self.ax.set_ylabel("Index")
+        #
+        # self.ax.set_title(f"Asx Index {self.step_count}")
 
     def render(self, mode='human'):
         img = get_img_from_fig(self.fig)
+        plt.close(self.fig)
         if mode == 'rgb_array':
             return img
         elif mode == 'human':
