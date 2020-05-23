77ee721c7d38d5f66dc26ea9e3ad75f7a4e2f5dc
James Shen <james.shen@guidebee.com>
2020-05-23 22:17:47 +0800

    add action and observation space

--- asx_gym/envs/asx_gym_env.py
+++ asx_gym/envs/asx_gym_env.py
@@ -6,8 +6,8 @@ import cv2
 from gym.envs.classic_control import rendering
 
 import matplotlib.pyplot as plt
-import gym
-from gym import spaces
+from gym import Env
+from gym import spaces, logger
 from gym.utils import seeding
 import sqlite3
 from gym.utils.colorize import *
@@ -33,17 +33,23 @@ def get_img_from_fig(fig, dpi=90):
     return img
 
 
-class AsxGymEnv(gym.Env):
+class AsxGymEnv(Env):
 
     def __init__(self, **kwargs):
         self.fig, self.ax = plt.subplots()
-        self.step_count = 0
         self.np_random, seed = seeding.np_random(0)
+        seed = seeding.create_seed(32)
+        self.seed(seed=seed)
+        self.step_count = 0
+
         self.viewer = rendering.SimpleImageViewer()
-        self.min_stock_date = date(2010, 5, 20)
+        self.min_stock_date = date(2011, 1, 1)
 
         # default values
-        self.start_date = kwargs.get('start_date', datetime(2012, 1, 1))
+        self.user_set_start_date = kwargs.get('start_date', self.min_stock_date)
+        if self.user_set_start_date < self.min_stock_date:
+            self.user_set_start_date = self.min_stock_date
+        self.start_date = self.user_set_start_date
         self.display_days = kwargs.get('display_days', 60)
         self.keep_same_company_when_reset = kwargs.get('keep_same_company_when_reset', True)
         self.keep_same_start_date_when_reset = kwargs.get('keep_same_start_date_when_reset', False)
@@ -53,6 +59,73 @@ class AsxGymEnv(gym.Env):
         self.expected_fund_increase_ratio = kwargs.get('expected_fund_increase_ratio', 2.0)
         self.expected_fund_decrease_ratio = kwargs.get('expected_fund_decrease_ratio', 0.2)
 
+        # company index start from 1, 0 means empty slot
+        self.max_company_number = 3000
+        self.max_stock_price = 100000
+        self.number_infinite = 10000000
+        self.random_start_days = 100
+        self.max_transaction_days = 0
+
+        # random start date
+        offset_days = self.np_random.randint(0, self.random_start_days)
+        self.start_date = self.user_set_start_date + timedelta(days=offset_days)
+
+        self.action_space = spaces.Dict(
+            {
+                "company_id": spaces.Discrete(self.max_company_number),
+                "buy_or_sell": spaces.Discrete(3),
+                "volume": spaces.Box(np.float32(0), high=np.float32(self.number_infinite), dtype=np.float32),
+                "price": spaces.Box(low=np.float32(0), high=np.float32(self.max_stock_price),
+                                    shape=(self.max_company_number,), dtype=np.float32),
+                "end_batch": spaces.Discrete(2)
+            }
+        )
+
+        self.observation_space = spaces.Dict(
+            {
+                "indexes": spaces.Dict({
+                    'open': spaces.Box(low=np.float32(0), high=np.float32(self.number_infinite),
+                                       dtype=np.float32),
+                    'close': spaces.Box(low=np.float32(0), high=np.float32(self.number_infinite),
+                                        dtype=np.float32),
+                    'high': spaces.Box(low=np.float32(0), high=np.float32(self.number_infinite),
+                                       dtype=np.float32),
+                    'low': spaces.Box(low=np.float32(0), high=np.float32(self.number_infinite),
+                                      dtype=np.float32),
+
+                }
+                ),
+                "day": spaces.Discrete(self.number_infinite),
+                "seconds": spaces.Discrete(24 * 3600),
+                "company_count": spaces.Discrete(self.max_company_number),
+                "prices:": spaces.Dict({
+                    "ask_price": spaces.Box(low=np.float32(0), high=np.float32(self.max_stock_price),
+                                            shape=(self.max_company_number,), dtype=np.float32),
+                    "bid_price": spaces.Box(low=np.float32(0), high=np.float32(self.max_stock_price),
+                                            shape=(self.max_company_number,), dtype=np.float32),
+                    "price": spaces.Box(low=np.float32(0), high=np.float32(self.max_stock_price),
+                                        shape=(self.max_company_number,), dtype=np.float32)}),
+
+                "portfolio_company_count": spaces.Discrete(self.max_company_number),
+                "portfolios": spaces.Dict(
+                    {
+                        "company_id": spaces.MultiDiscrete([self.max_company_number] * self.max_company_number),
+
+                        "volume": spaces.Box(np.float32(0), high=np.float32(self.number_infinite),
+                                             shape=(self.max_company_number,),
+                                             dtype=np.float32),
+                        "buy_price": spaces.Box(low=np.float32(0), high=np.float32(self.max_stock_price),
+                                                shape=(self.max_company_number,), dtype=np.float32),
+                        "sell_price": spaces.Box(low=np.float32(0), high=np.float32(self.max_stock_price),
+                                                 shape=(self.max_company_number,), dtype=np.float32),
+                    }),
+                "total_value:": spaces.Box(low=np.float32(0), high=np.float32(self.number_infinite), dtype=np.float32),
+                "available_fund:": spaces.Box(low=np.float32(0), high=np.float32(self.number_infinite),
+                                              dtype=np.float32)
+
+            }
+        )
+
         print(colorize("Initializing data, it may take a couple minutes,please wait...", 'red'))
         db_file = f'{pathlib.Path().absolute()}/asx_gym/db.sqlite3'
         conn = sqlite3.connect(db_file)
@@ -61,6 +134,12 @@ class AsxGymEnv(gym.Env):
         updated_date = cur.fetchone()
         updated_date = updated_date[0]
         self.max_stock_date = datetime.strptime(updated_date, date_fmt).date()
+
+        if self.user_set_start_date > self.max_stock_date + timedelta(days=-100):
+            self.user_set_start_date = self.min_stock_date + timedelta(days=-100)
+            self.start_date = self.user_set_start_date
+
+        self.max_transaction_days = (self.max_stock_date - self.min_stock_date).days
         print(colorize(f"Stock date range from {self.min_stock_date} to {self.max_stock_date}", "blue"))
         print(colorize("reading asx index data", 'blue'))
         self.index_df = pd.read_sql_query(
@@ -82,8 +161,12 @@ class AsxGymEnv(gym.Env):
         # print(f'Asx stock data records:\n{self.price_df.count()}')
         conn.close()
         print(colorize("Data initialized", "green"))
-        if kwargs['start_date']:
-            self.start_date = kwargs['start_date']
+
+    def history_indexes(self, days=-1):
+        pass
+
+    def history_stock_prices(self, days=-1):
+        pass
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -100,8 +183,8 @@ class AsxGymEnv(gym.Env):
         return self.step_count, 0, done, {}
 
     def draw_stock(self):
-        start_date = self.start_date + timedelta(days=self.step_count)
-        end_date = self.start_date + timedelta(days=self.step_count + self.display_days)
+        end_date = self.start_date + timedelta(days=self.step_count)
+        start_date = self.start_date + timedelta(days=self.step_count - self.display_days)
         stock_index = self.index_df.loc[start_date:end_date]
         display_date = end_date.strftime(date_fmt)
         self.fig, self.ax = mpf.plot(stock_index,
@@ -112,6 +195,18 @@ class AsxGymEnv(gym.Env):
                                      )
 
     def reset(self):
+
+        # try to close exist fig if possible
+        try:
+            plt.close(self.fig)
+        except:
+            pass
+
+        if not self.keep_same_start_date_when_reset:
+            offset_days = self.np_random.randint(0, self.random_start_days)
+            self.start_date = self.user_set_start_date + timedelta(days=offset_days)
+
+        logger.info(f'Reset date to {self.start_date}')
 
         self.step_count = 0
         self.draw_stock()
