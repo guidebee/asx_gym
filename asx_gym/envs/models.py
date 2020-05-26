e9f18e9e10580434671d7730902dfbc558c30710
James Shen <james.shen@guidebee.com>
2020-05-26 08:55:26 +0800

    draft simulation data

--- asx_gym/envs/models.py
+++ asx_gym/envs/models.py
@@ -1,3 +1,6 @@
+import random
+
+
 class TransactionFee(object):
     def __init__(self, amount, fee, is_percentage=False):
         self.amount = amount
@@ -19,16 +22,20 @@ class StockDailySimulationPrices(object):
         self.close_price = close_price
         self.high_price = high_price
         self.low_price = low_price
+        self.offset = 0
         self.current_index = 0
         self.simulation_prices = []
         first_price = StockSimulationPrice(open_price, open_price, open_price)
         self.simulation_prices.append(first_price)
 
     def get_next_prices(self):
+        if self.current_index <= self.offset:
+            return self.simulation_prices[0]
         count = len(self.simulation_prices)
-        if self.current_index > count - 1:
-            self.current_index = count - 1
-        ret_price = self.simulation_prices[self.current_index]
+        index = self.current_index - self.offset
+        if index > count - 1:
+            index = count - 1
+        ret_price = self.simulation_prices[index]
         self.current_index += 1
         return ret_price
 
@@ -43,3 +50,7 @@ class StockDailySimulationPrices(object):
 
         last_price = StockSimulationPrice(self.close_price, self.close_price, self.close_price)
         self.simulation_prices.append(last_price)
+        count = len(self.simulation_prices)
+        empty_count = max(24 - count - 1, 0)
+        if empty_count > 0:
+            self.offset = random.randint(1, empty_count)
